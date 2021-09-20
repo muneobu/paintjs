141d21c7a4dc33736c4b59e272eb0a98022da479
muneobu <muneobu@gmail.com>
2021-09-20 21:18:44 +0900

    :fearful:

---
+++ app.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  console.log(x, y);
+}
+
+function onMouseDown(event) {
+  startPainting();
+}
+
+function onMouseUp(event) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
