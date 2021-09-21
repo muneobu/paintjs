e1943145a44fd082e86c7d536366330f818ebac3
muneobu <muneobu@gmail.com>
2021-09-21 12:21:20 +0900

    :wink:

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let closepth = false;
 
 function stopPainting() {
   painting = false;
@@ -12,20 +19,39 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    console.log("creating path in", x, y);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    console.log("creating line in", x, y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    if (closepth) {
+      ctx.closePath();
+    }
+  }
 }
 
 function onMouseDown(event) {
   startPainting();
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    console.log("space");
+    closepth = true;
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    console.log("spaceUp");
+    closepth = false;
+  }
+});
